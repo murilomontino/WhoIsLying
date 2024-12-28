f90da6615a47707d3cd9dde223694be77b797453
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-28 15:59:26 -0300

    chore: adiciona configuração do NativeWind e estrutura inicial do projeto com Expo

---
+++ app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Slot } from 'expo-router'
+
+// Import your global CSS file
+import '../global.css'
+
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function _Layout() {
+    return (
+        <SafeAreaView
+            edges={['top']}
+            className="flex flex-1 w-full"
+            style={{ backgroundColor: '#09b285' }}
+        >
+                <StatusBar backgroundColor="#09b285" translucent animated />
+                <Slot />
+        </SafeAreaView>
+    )
+}
