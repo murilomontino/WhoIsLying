7221a9e09b960acc5e7d809d8718865350785faf
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-29 20:57:14 -0300

    feat: adiciona suporte para estado desabilitado nos botões

--- source/components/atoms/button/button.tsx
+++ source/components/atoms/button/button.tsx
@@ -7,16 +7,50 @@ const button = tv({
     base: `
         bg-blue-500 px-4 py-8 w-full rounded-full items-center justify-center h-12
     `,
+    variants: {
+        disabled: {
+            true: 'bg-gray-500 opacity-70', // Aplica um tom de cinza quando desabilitado
+        },
+    },
 })
 
-export function Button({ className, ...props }: ButtonProps) {
-    return <TouchableOpacity {...props} />
+const buttonWithoutClass = tv({
+    base: `
+        
+    `,
+    variants: {
+        disabled: {
+            true: 'bg-gray-500 opacity-70 rounded-lg', // Aplica um tom de cinza quando desabilitado
+        },
+    },
+})
+
+export function Button({ className, disabled, ...props }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            {...props}
+            disabled={disabled}
+            className={buttonWithoutClass({ className, disabled })}
+        />
+    )
 }
 
-export function ButtonPrimary({ className, ...props }: ButtonProps) {
-    return <TouchableOpacity {...props} className={button({ className })} />
+export function ButtonPrimary({ className, disabled, ...props }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            {...props}
+            disabled={disabled}
+            className={button({ className, disabled })}
+        />
+    )
 }
 
-export function ButtonSecondary({ className, ...props }: ButtonProps) {
-    return <TouchableOpacity {...props} className={button({ className })} />
+export function ButtonSecondary({ className, disabled, ...props }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            {...props}
+            disabled={disabled}
+            className={button({ className, disabled })}
+        />
+    )
 }
