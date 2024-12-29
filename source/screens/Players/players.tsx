95e9b4fb11d062875184f6ea74586048007938fb
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-29 20:00:27 -0300

    feat: adiciona funcionalidade de deletar jogadores e aprimora a interface de exibição

--- source/screens/Players/players.tsx
+++ source/screens/Players/players.tsx
@@ -8,7 +8,7 @@ import { ButtonPrimary } from '~/components/atoms/button/button'
 import Title from '~/components/atoms/title'
 import ControlInput from '~/components/molecules/control-input'
 import { useAppDispatch, useAppSelector } from '~/store/hooks'
-import { onAddPlayers } from '~/store/slices/players/actions'
+import { onAddPlayers, onDeletePlayers } from '~/store/slices/players/actions'
 
 const schema = Yup.object().shape({
     name: Yup.string().trim().required('Required'),
@@ -21,6 +21,7 @@ export default function Page() {
     const {
         control,
         handleSubmit,
+        resetField,
         formState: { errors, isValid },
     } = useForm({
         resolver: yupResolver(schema),
@@ -28,15 +29,35 @@ export default function Page() {
 
     const handlePress = (data: { name: string }) => {
         dispatch(onAddPlayers(data))
+        resetField('name')
+    }
+
+    const handleDelete = (id: string) => {
+        dispatch(onDeletePlayers({ id }))
     }
 
     return (
         <View className="flex items-center justify-start flex-1 py-4 space-y-8 bg-emerald-400">
             <Title />
-
-            {players.map((player) => (
-                <Text key={player._id}>{player.name}</Text>
-            ))}
+            <View className="flex flex-col w-1/2 space-y-4">
+                {players.map((player) => (
+                    <View
+                        key={player._id}
+                        className="flex flex-row items-center flex-1 px-4 py-2 space-x-4 bg-white rounded-full"
+                    >
+                        <Text className="text-2xl flex-[4] text-center text-gray-800">
+                            {player.name}
+                        </Text>
+                        <Button onPress={() => handleDelete(player._id)}>
+                            <AntDesign
+                                name="delete"
+                                size={24}
+                                className="text-gray-800"
+                            />
+                        </Button>
+                    </View>
+                ))}
+            </View>
 
             <View className="flex flex-row items-center justify-center w-full space-x-4">
                 <ControlInput
@@ -56,10 +77,13 @@ export default function Page() {
             <View className="flex flex-row items-center justify-center w-full space-x-4">
                 <ButtonPrimary className="w-1/2">
                     <Text
-                        className="text-gray-800"
+                        className="text-white"
                         style={{
                             fontFamily: 'Bangers_400Regular',
                             fontSize: 42,
+                            textShadowColor: 'black', // Cor da borda
+                            textShadowOffset: { width: 2, height: 2 }, // Offset da sombra
+                            textShadowRadius: 2, // Raio para suavizar a sombra
                         }}
                     >
                         Start Game
