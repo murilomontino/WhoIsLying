9b60a5e733828edd023392d0c1b4ae70677469d1
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-29 11:41:49 -0300

    feat: adiciona componente de input com suporte a fontes personalizadas

---
+++ source/components/atoms/input/input.tsx
@@ -0,0 +1,31 @@
+import { Poppins_400Regular, useFonts } from '@expo-google-fonts/poppins'
+import { TextInput, type TextInputProps } from 'react-native'
+import { tv } from 'tailwind-variants'
+
+export type InputProps = TextInputProps
+
+const input = tv({
+    base: `
+        border border-gray-400 rounded-full px-4 py-2 bg-white text-gray-600
+    `,
+})
+
+export default function Input({ className, ...props }: InputProps) {
+    const [fontsLoaded] = useFonts({
+        Poppins_400Regular,
+    })
+
+    if (!fontsLoaded) {
+        return null
+    }
+
+    return (
+        <TextInput
+            {...props}
+            className={input({ className })}
+            style={{
+                fontFamily: 'Poppins_400Regular',
+            }}
+        />
+    )
+}
