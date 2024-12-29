5592bcea759dfaae426dd7ea5221c751f5cf82c8
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-29 20:00:23 -0300

    feat: aprimora componente Title com animação de texto e sombra

--- source/components/atoms/title/title.tsx
+++ source/components/atoms/title/title.tsx
@@ -1,9 +1,11 @@
 import { Bangers_400Regular, useFonts } from '@expo-google-fonts/bangers'
-import { Text, type TextProps } from 'react-native'
+import { Text, type TextProps, View } from 'react-native'
 
 export type TitleProps = TextProps
 
 export default function Title({ className, ...props }: TitleProps) {
+    const title = 'Who Is Lying'
+
     const [fontsLoaded] = useFonts({
         Bangers_400Regular,
     })
@@ -13,15 +15,26 @@ export default function Title({ className, ...props }: TitleProps) {
     }
 
     return (
-        <Text
-            {...props}
-            style={{
-                fontFamily: 'Bangers_400Regular',
-                fontStyle: 'italic',
-                fontSize: 82,
-            }}
-        >
-            Who Is Lying
-        </Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+            {title.split('').map((char: string, index: number) => (
+                <Text
+                    key={index}
+                    style={{
+                        fontFamily: 'Bangers_400Regular',
+                        fontStyle: 'italic',
+                        fontSize: 82,
+                        color: 'white',
+                        textShadowColor: 'black', // Cor da borda
+                        textShadowOffset: { width: 2, height: 2 }, // Offset da sombra
+                        textShadowRadius: 2, // Raio para suavizar a sombra
+                        marginRight: 4,
+                        transform: [{ translateY: index % 2 === 0 ? -2 : 2 }], // Alterna posição no eixo Y
+                    }}
+                    {...props}
+                >
+                    {char}
+                </Text>
+            ))}
+        </View>
     )
 }
