45eb0cab586374427a686f4854fecdd561e54cbd
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-30 11:23:33 -0300

    feat: refatora a classe Player para um objeto com métodos de manipulação e novas propriedades

--- source/store/slices/players/player.ts
+++ source/store/slices/players/player.ts
@@ -1,38 +1,97 @@
-export class Player {
+export const Player = {
+    _id: '',
+    name: '',
+    score: 0,
+    reveal: false,
+    canAnswer: true,
+    canAsk: true,
+    __protocol: 'player',
+
+    // Métodos de manipulação
+    addScore(score: number) {
+        this.score += score
+        return this
+    },
+
+    removeScore(score: number) {
+        this.score -= score
+        return this
+    },
+
+    defineScore(score: number) {
+        this.score = score
+        return this
+    },
+
+    revealPlayer() {
+        this.reveal = true
+        return this
+    },
+
+    hidePlayer() {
+        this.reveal = false
+        return this
+    },
+
+    enableAnswer() {
+        this.canAnswer = true
+        return this
+    },
+
+    disableAnswer() {
+        this.canAnswer = false
+        return this
+    },
+
+    enableAsk() {
+        this.canAsk = true
+        return this
+    },
+
+    disableAsk() {
+        this.canAsk = false
+        return this
+    },
+
+    defineName(name: string) {
+        this.name = name.trim()
+        return this
+    },
+
+    defineId(id: string) {
+        this._id = id
+        return this
+    },
+
+    defineReveal(reveal: boolean) {
+        this.reveal = reveal
+        return this
+    },
+
+    defineCanAnswer(canAnswer: boolean) {
+        this.canAnswer = canAnswer
+        return this
+    },
+
+    defineCanAsk(canAsk: boolean) {
+        this.canAsk = canAsk
+        return this
+    },
+
+    defineProtocol(protocol: string) {
+        this.__protocol = protocol
+        return this
+    },
+}
+
+export type IPlayer = {
     _id: string
     name: string
     score: number
     reveal: boolean
-
-    constructor(id: string, name: string) {
-        this._id = id
-        this.name = name.trim()
-        this.score = 0
-        this.reveal = false
-    }
-
-    addScore(score: number): this {
-        this.score += score
-        return this
-    }
-
-    removeScore(score: number): this {
-        this.score -= score
-        return this
-    }
-
-    defineScore(score: number): this {
-        this.score = score
-        return this
-    }
-
-    revealPlayer(): this {
-        this.reveal = true
-        return this
-    }
-
-    hidePlayer(): this {
-        this.reveal = false
-        return this
-    }
+    canAnswer: boolean
+    canAsk: boolean
+    __protocol: string
 }
+
+export const build = (player: IPlayer) => Object.assign({}, Player, player)
