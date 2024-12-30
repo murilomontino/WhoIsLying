57c26ce3c16471d883716661ae818d0d0bb030f3
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-30 10:25:54 -0300

    feat: adiciona nova tela Asking com componente básico

---
+++ app/asking/index.tsx
@@ -0,0 +1,3 @@
+import Page from '~/screens/Asking'
+
+export default Page
---
+++ source/screens/Asking/Asking.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Asking = () => {
+    return (
+        <View>
+            <Text>Asking</Text>
+        </View>
+    )
+}
+
+export default Asking
