3895c9203231d1d62c937a01dc5d1acb53ca141c
Murilo Montino <murilomontinojr@hotmail.com>
2024-12-31 11:37:10 -0300

    ref: adiciona suporte a fontes personalizadas e sombras de texto no Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // NOTE: Update this to include the paths to all of your component files.
-    content: [
-        './source/**/*.{js,jsx,ts,tsx}',
-        './app/**/*.{js,jsx,ts,tsx}',
-    ],
+    content: ['./source/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
     presets: [require('nativewind/preset')],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                bangers: ['Bangers_400Regular', 'sans-serif'], // Adicione a fonte desejada
+                poppins: ['Poppins_400Regular', 'sans-serif'], // Adicione a fonte desejada
+            },
+            textShadow: {
+                outlined: '2px 2px 2px #000',
+                'outlined-white': '2px 2px 2px #fff',
+                'outlined-red': '2px 2px 2px #ef4444',
+            },
+        },
     },
-    plugins: [],
+    plugins: [require('tailwindcss-textshadow')],
 }
