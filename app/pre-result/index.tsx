de7c4ddcbc54c57d24f3b736b49e93d14e90ac27
Murilo Montino <murilomontinojr@hotmail.com>
2025-01-02 14:39:08 -0300

    feat: adiciona tela de pré-resultados com navegação para a tela de resultados

---
+++ app/pre-result/index.tsx
@@ -0,0 +1,3 @@
+import Page from '~/screens/PreResult'
+
+export default Page
---
+++ source/screens/PreResult/PreResult.tsx
@@ -0,0 +1,72 @@
+import { useRouter } from 'expo-router'
+import { BounceIn, BounceOut, FadeIn, FadeOut } from 'react-native-reanimated'
+import DefaultLayout from '~/components/_layout/default'
+import { ButtonPrimary } from '~/components/atoms/button'
+import Text from '~/components/atoms/text'
+import Title from '~/components/atoms/title'
+import View from '~/components/ui/view'
+
+const PreResultScreen = () => {
+    const router = useRouter()
+
+    const handleContinue = () => {
+        router.push('/result')
+    }
+
+    return (
+        <DefaultLayout>
+            <View
+                delay={100}
+                entering={FadeIn}
+                exiting={FadeOut}
+                className="flex flex-col items-center w-full h-full justify-evenly"
+            >
+                <View
+                    delay={100}
+                    entering={FadeIn}
+                    exiting={FadeOut}
+                    className="flex flex-col items-center justify-center w-full px-2 space-y-4"
+                >
+                    <Title />
+                    <Text
+                        entering={FadeIn.duration(1000)}
+                        exiting={FadeOut.duration(1000)}
+                        as="h2"
+                        className="text-center !text-white text-shadow-outlined-red"
+                    >
+                        Resultado da Votação
+                    </Text>
+                </View>
+
+                <Text
+                    delay={100}
+                    entering={FadeIn.duration(1000)}
+                    as="h2"
+                    className="text-center !text-white text-shadow-outlined"
+                >
+                    Mostre esta tela para todos
+                </Text>
+                <View
+                    delay={100}
+                    entering={BounceIn.duration(1000)}
+                    exiting={BounceOut.duration(1000)}
+                    className="flex flex-row items-center justify-center w-full px-4 space-x-4"
+                >
+                    <ButtonPrimary
+                        onPress={handleContinue}
+                        className="w-full md:w-1/2"
+                    >
+                        <Text
+                            as="h3"
+                            className="!text-white text-shadow-outlined-red"
+                        >
+                            Ver Resultado
+                        </Text>
+                    </ButtonPrimary>
+                </View>
+            </View>
+        </DefaultLayout>
+    )
+}
+
+export default PreResultScreen
