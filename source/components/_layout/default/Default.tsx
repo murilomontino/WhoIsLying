04267a8e3fb86a95401fcd6e0db89d1e575440a7
Murilo Montino <murilomontinojr@hotmail.com>
2025-01-03 16:33:06 -0300

    ref: adiciona suporte a classes CSS personalizadas no componente DefaultLayout

--- source/components/_layout/default/Default.tsx
+++ source/components/_layout/default/Default.tsx
@@ -1,13 +1,20 @@
+import cn from 'classnames'
 import type React from 'react'
 import { View } from 'react-native'
 
 type DefaultLayoutProps = {
     children: React.ReactNode
+    className?: string
 }
 
-const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+const DefaultLayout = ({ children, className }: DefaultLayoutProps) => {
     return (
-        <View className="flex items-center justify-start flex-1 py-4 space-y-4 overflow-y-auto bg-emerald-400">
+        <View
+            className={cn(
+                'flex items-center justify-start flex-1 py-4 space-y-4 overflow-y-auto bg-emerald-400',
+                className,
+            )}
+        >
             {children}
         </View>
     )
