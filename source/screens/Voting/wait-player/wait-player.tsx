41b07a695ef2cc78c6d8cb423f3991e1df8a73b9
Murilo Montino <murilomontinojr@hotmail.com>
2025-01-03 09:02:17 -0300

    ref: adiciona animações de entrada e saída na tela WaitPlayer com controle de desmontagem

--- source/screens/Voting/wait-player/wait-player.tsx
+++ source/screens/Voting/wait-player/wait-player.tsx
@@ -1,16 +1,27 @@
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import React, { useEffect, useState } from 'react'
-import { FadeIn, FadeOut } from 'react-native-reanimated'
+import {
+    BounceInLeft,
+    BounceInRight,
+    BounceOutLeft,
+    BounceOutRight,
+    FadeIn,
+    FadeInDown,
+    FadeOut,
+    FadeOutDown,
+} from 'react-native-reanimated'
 import DefaultLayout from '~/components/_layout/default'
 import { ButtonSecondary } from '~/components/atoms/button'
 import Text from '~/components/atoms/text'
 import View from '~/components/ui/view'
 import { useAppSelector } from '~/store/hooks'
 import type { Player } from '~/store/slices/players/player'
+import { delay } from '~/utils/delay'
 
 const WaitPlayerScreen = () => {
     const [player, setPlayer] = useState<typeof Player | null>(null)
     const [isMounted, setIsMounted] = useState(false)
+    const [isExiting, setIsExiting] = useState(false)
     const router = useRouter()
     const { players } = useAppSelector((state) => state.players)
     const { id } = useLocalSearchParams()
@@ -29,39 +40,52 @@ const WaitPlayerScreen = () => {
         setIsMounted(true)
     }, [])
 
-    const handleReveal = () => {
+    const handleReveal = async () => {
+        setIsExiting(true)
+        await delay(1000)
         router.navigate(`/voting/${player?._id}`)
     }
 
     return (
         <DefaultLayout>
             <View
-                entering={FadeIn}
-                exiting={FadeOut}
+                delay={100}
+                demount={isExiting}
+                entering={FadeIn.duration(500)}
+                exiting={FadeOut.duration(500)}
                 className="flex flex-col items-center justify-center w-full h-full space-y-8"
             >
-                <View className="flex flex-col items-center justify-center w-full px-2 space-y-4">
+                <View
+                    exiting={FadeOut.duration(500)}
+                    demount={isExiting}
+                    className="flex flex-col items-center justify-center w-full px-2 space-y-4"
+                >
                     <Text
-                        className="text-center text-white"
-                        style={{
-                            fontFamily: 'Bangers_400Regular',
-                            fontSize: 42,
-                            textShadowColor: '#ef4444', // Cor da borda
-                            textShadowOffset: { width: 2, height: 2 }, // Offset da sombra
-                            textShadowRadius: 2, // Raio para suavizar a sombra
-                        }}
+                        demount={isExiting}
+                        entering={BounceInLeft.duration(500)}
+                        exiting={BounceOutRight.duration(500)}
+                        as="h4"
+                        className="text-center text-gray-800"
                     >
                         Passe para o(a){' '}
                     </Text>
                     <Text
+                        demount={isExiting}
                         as="h1"
+                        entering={BounceInRight.duration(500)}
+                        exiting={BounceOutLeft.duration(500)}
                         className="!text-gray-800 text-shadow-outlined-red"
                     >
                         {player?.name}
                     </Text>
                 </View>
 
-                <View className="flex items-center justify-center w-full px-8">
+                <View
+                    demount={isExiting}
+                    entering={FadeInDown.duration(500)}
+                    exiting={FadeOutDown.duration(500)}
+                    className="flex items-center justify-center w-full px-8"
+                >
                     <ButtonSecondary
                         onPress={handleReveal}
                         className="w-full rounded-full md:w-1/2"
