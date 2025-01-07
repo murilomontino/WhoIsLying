c7742a16f2409f69e32fd337540a91190dd415d1
Murilo Montino <murilomontinojr@hotmail.com>
2025-01-07 17:34:43 -0300

    ref: adiciona suporte a sombras nos botões e ajusta estilos

--- source/components/atoms/button/button.tsx
+++ source/components/atoms/button/button.tsx
@@ -3,29 +3,40 @@ import {
     TouchableOpacity,
     type TouchableOpacityProps,
 } from 'react-native'
+import { Shadow } from 'react-native-shadow-2'
 import { tv } from 'tailwind-variants'
 import withControl, { type ControlProps } from '~/components/helpers/with-control'
 import withDelay from '~/components/helpers/with-delay'
 import useSound from '~/components/hooks/use-sound'
+type ShadowProps = {
+    shadowColor: string
+    shadowOffset: {
+        width: number
+        height: number
+    }
+    shadowOpacity: number
+    shadowRadius: number
+    elevation: number
+}
 
 type ButtonProps = TouchableOpacityProps & {
     delay?: number
     hasSound?: boolean
+    shadow?: ShadowProps | null
 } & ControlProps
 
 const button = tv({
     base: `
-        shadow-lg opacity-90 hover:opacity-100 transition-opacity disabled:hover:opacity-70
-        px-4 py-8 w-full rounded-full items-center justify-center h-12 border-4 text-4xl
-        font-bold font-bangers transition-colors transition duration-300
+        opacity-90 hover:opacity-100 disabled:hover:opacity-70
+        px-4 py-1 md:py-8 rounded-full items-center justify-center md:h-12 
+        border-2 border-red-400 w-full
     `,
     variants: {
         disabled: {
             true: '!bg-gray-500 !border-gray-200 opacity-40', // Aplica um tom de cinza quando desabilitado
         },
         color: {
-            primary:
-                'bg-blue-500 border-red-500 !text-white text-shadow-outlined-red',
+            primary: 'bg-blue-500  !text-white',
             secondary: 'bg-white border-red-500',
         },
     },
@@ -42,7 +53,13 @@ const buttonWithoutClass = tv({
     },
 })
 
-function Btn({ className, disabled, hasSound = true, ...props }: ButtonProps) {
+function Btn({
+    className,
+    shadow,
+    disabled,
+    hasSound = true,
+    ...props
+}: ButtonProps) {
     const { playClickSound } = useSound({ sound: 'click', volume: 0.2 })
 
     const handlePress = (event: GestureResponderEvent) => {
@@ -55,6 +72,7 @@ function Btn({ className, disabled, hasSound = true, ...props }: ButtonProps) {
         <TouchableOpacity
             {...props}
             onPress={handlePress}
+            style={[props.style, shadow]}
             disabled={disabled}
             className={buttonWithoutClass({ className, disabled })}
         />
@@ -65,20 +83,34 @@ export const Button = withControl(withDelay(Btn))
 
 export function ButtonPrimary({ className, disabled, ...props }: ButtonProps) {
     return (
-        <Button
-            {...props}
-            disabled={disabled}
-            className={button({ className, disabled, color: 'primary' })}
-        />
+        <Shadow
+            distance={7}
+            startColor={'#ef4444'}
+            style={[{ borderRadius: 100 }]}
+            offset={[1, 1]}
+        >
+            <Button
+                {...props}
+                disabled={disabled}
+                className={button({ className, disabled, color: 'primary' })}
+            />
+        </Shadow>
     )
 }
 
 export function ButtonSecondary({ className, disabled, ...props }: ButtonProps) {
     return (
-        <Button
-            {...props}
-            disabled={disabled}
-            className={button({ className, disabled, color: 'secondary' })}
-        />
+        <Shadow
+            distance={10}
+            startColor={'#ef4444'}
+            style={{ borderRadius: 100 }}
+            offset={[1, 1]}
+        >
+            <Button
+                {...props}
+                disabled={disabled}
+                className={button({ className, disabled, color: 'secondary' })}
+            />
+        </Shadow>
     )
 }
